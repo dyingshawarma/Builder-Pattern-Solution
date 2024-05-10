8ca45b7b6bbd912072ee2d981b1980c4cc0071e5
Charlene Manning <113045523+dyingshawarma@users.noreply.github.com>
2024-05-11 01:02:34 +0800

    Create UML Diagram.java

---
+++ UML Diagram.java
@@ -0,0 +1,21 @@
++----------------+
+|   User         |
++----------------+
+| - firstName    |
+| - lastName     |
+| - email        |
+| - address      |
+| - phone        |
+| - age          |
++----------------+
++----------------+
+| UserBuilder    |
++----------------+
+| + setFirstName |
+| + setLastName  |
+| + setEmail     |
+| + setAddress   |
+| + setPhone     |
+| + setAge       |
+| + build()      |
++----------------+
